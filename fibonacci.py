5e5d2d76dc6941cbcb0603286e911c446c30d547
seyeonyy <123892130+seyeonyy@users.noreply.github.com>
2023-02-07 17:27:01 +0900

    Delete fibonacci.py

--- fibonacci.py
+++
@@ -1,9 +0,0 @@
-def fibonacci(index):
-    if index == 0:
-        return 0
-    elif index == 1:
-        return 1
-    return fibonacci(index-2) + fibonacci(index-1)
-
-index = int(input())
-print(fibonacci(index))
